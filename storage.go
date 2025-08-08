9e0c5fe651feab86193138692e7ddf73ca7f9b21
yeshey tenzin <yesheytenzin0@gmail.com>
2025-08-08 23:33:29 +0600

    reconfigure incomplete

--- storage.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import (
-	"encoding/json"
-	"io/ioutil"
-	"os"
-)
-
-func saveTasks(){}
--- task.go
+++
@@ -1,5 +0,0 @@
-type Task struct {
-	ID        int			`json:"id"`
-	Title			string 	`json:"title"`
-	completed bool 		`json:"completed"`
-}
